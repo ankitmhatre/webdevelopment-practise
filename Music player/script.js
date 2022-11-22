f3b0b7eb62dca06d4a5aa9cb35fea62c01607633
Ankit <ankitmhatre.mhatre9@gmail.com>
2022-11-22 10:14:39 -0500

    repeat image

--- Music player/script.js
+++ Music player/script.js
@@ -7,79 +7,45 @@ console.log("this is amazing");
 // run();
 // console.log(run);
 // initialize the variables
-let songs = [
-  {
-    songName: "Beetein Lamhe",
-    filePath: "./assets/music/1.mp3",
-    coverPath: "./assets/cover/1.jpg",
-  },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-  //   {
-  //     songName: "BeeteinLamhe",
-  //     filePath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs",
-  //     coverPath:
-  //       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
-  //   },
-];
 
+var currentPlayingSongIndex = 0;
+var repeatState = 0;
 
-
-songs.forEach(k=>{
-  console.log(k)
-})
-
-
-let audioElement = new Audio(songs[0].filePath);
-
-
-console.log(audioElement);
 let songIndex = 0; //no song play initially
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let pause = document.getElementById("pausebtn");
 let gif = document.getElementById("gif");
+let repeatElement = document.getElementById("repeat");
+
+// {
+//0 = repeatAll,
+// 1 = repeatOne,
+// 2 = repeatNone -not using
+//}
+
+let songs = [
+  {
+    songName: "Beetein Lamhe 1 ",
+    filePath: "./assets/music/1.mp3",
+    coverPath: "./assets/cover/1.jpg",
+  },
+  {
+    songName: "BeeteinLamhe 2",
+    filePath: "./assets/music/1.mp3",
+    coverPath: "./assets/cover/1.jpg",
+  },
+];
+
+checkRepeatState(0);
+
+songs.forEach((k) => {
+  console.log(k);
+});
+
+let audioElement = new Audio(songs[currentPlayingSongIndex].filePath);
+
+console.log(audioElement);
 
 //Listen to events
 myProgressBar.addEventListener("timeupdate", function () {});
@@ -90,16 +56,46 @@ masterPlay.addEventListener("click", function () {
     audioElement.play();
     masterPlay.classList.remove("play");
     masterPlay.classList.add("pause");
-    masterPlay.src="./Icons/Pause.svg";
+    masterPlay.src = "./Icons/Pause.svg";
     gif.style.opacity = 1;
   } else {
     audioElement.pause();
     masterPlay.classList.remove("pause");
     masterPlay.classList.add("play");
-    masterPlay.src="./Icons/Play.svg";
+    masterPlay.src = "./Icons/Play.svg";
     gif.style.opacity = 0;
   }
 });
 
-console.log(masterPlay);
-console.log(masterPlay.classList.add("pause"));
+
+
+repeatElement.addEventListener("click", ()=>{
+  if(repeatState==1){
+    repeatState=0;
+  }else{
+    repeatState++;
+  }
+
+ checkRepeatState()
+})
+
+
+function checkRepeatState() {
+  
+
+  switch (repeatState) {
+    case 0:
+      repeatElement.src = "./Icons/repeat.svg";
+
+      break;
+
+    case 1:
+      repeatElement.src = "./Icons/repeat-once.png";
+
+      break;
+
+      default:
+        repeatElement.src = "./Icons/repeat.svg";
+
+  }
+}
