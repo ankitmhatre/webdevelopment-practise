25fc84924b9cd20d7c16967211bde74b40949a5e
Uday <udayvasave94@gmail.com>
2022-11-22 19:49:30 +0530

    songadded

--- Music player/script.js
+++ Music player/script.js
@@ -1,11 +1,16 @@
 console.log("this is amazing");
-
+// function run() {
+//   var URL =
+//     "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerSongs\1.mp3";
+//   window.open(URL, null);
+// }
+// run();
+// console.log(run);
 // initialize the variables
 let songs = [
   {
     songName: "BeeteinLamhe",
-    filePath:
-      "D:AcademicJavascriptjs/practicewebdevelopment-practiseMusic/playerSongs",
+    filePath: "1.mp3",
     coverPath:
       "D:AcademicJavascriptjs practicewebdevelopment-practiseMusic playerCover Images",
   },
@@ -60,22 +65,32 @@ let songs = [
   //   },
 ];
 
-let audioElement = new Audio(
-  "D:/Academic/Javascript/js practice/webdevelopment-practise/Music player/Songs"
-);
+let audioElement = new Audio("1.mp3");
 
 console.log(audioElement);
 let songIndex = 0; //no song play initially
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
+let pause = document.getElementById("pausebtn");
+let gif = document.getElementById("gif");
 
 //Listen to events
 myProgressBar.addEventListener("timeupdate", function () {});
 
 //play pause click
-
 masterPlay.addEventListener("click", function () {
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
+    masterPlay.classList.remove("play");
+    masterPlay.classList.add("pause");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("pause");
+    masterPlay.classList.add("play");
+    gif.style.opacity = 0;
   }
 });
+
+console.log(masterPlay);
+console.log(masterPlay.classList.add("pause"));
